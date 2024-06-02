49cf8be721d29278ff32d2f5139f1725bda83603
Stanislao Tariffa <stany.tariffa@gmail.com>
2024-06-02 13:08:11 +0200

    first commit

---
+++ src/main/java/org/generation/italy/reshare/ReshareApplication.java
@@ -0,0 +1,13 @@
+package org.generation.italy.reshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReshareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReshareApplication.class, args);
+	}
+
+}
