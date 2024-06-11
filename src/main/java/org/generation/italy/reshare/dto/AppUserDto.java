c9ccefd7c2b8ed8f937ee99d3131992092e6e550
Francesca Piccitto <framari.p@gmail.com>
2024-06-11 17:26:07 +0200

    aggiunge CategoryDto e AppUserDto

---
+++ src/main/java/org/generation/italy/reshare/dto/AppUserDto.java
@@ -0,0 +1,35 @@
+package org.generation.italy.reshare.dto;
+
+import org.generation.italy.reshare.model.AppUser;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+public class AppUserDto {
+    private int id;
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String phone;
+    private String gender;
+    private String birthdate;
+    private String description;
+
+    public AppUserDto() {}
+
+    public AppUserDto(AppUser appUser) {
+        this.id = appUser.getId();
+        this.firstname = appUser.getFirstname();
+        this.lastname = appUser.getLastname();
+        this.email = appUser.getEmail();
+        this.phone = appUser.getPhone();
+        this.gender = appUser.getGender();
+        this.birthdate = appUser.getBirthdate().format(DateTimeFormatter.ofPattern("d-MMM-uuuu"));
+        this.description = appUser.getDescription();
+    }
+
+    public AppUser toAppUser(){
+        return new AppUser(this.id, this.firstname, this.lastname, this.email, this.phone, this.gender, LocalDate.parse(this.birthdate, DateTimeFormatter.ofPattern("uuuu-MM-dd")), this.description);
+    }
+}
