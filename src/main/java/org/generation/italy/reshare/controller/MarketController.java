95396f15c6555d115e6fa12c4d3989d87b6fbebd
Lorenzo Murra <lorenzo.murra@gmail.com>
2024-06-13 14:48:24 +0200

    aggiunge WebConfig

--- src/main/java/org/generation/italy/reshare/controller/MarketController.java
+++ src/main/java/org/generation/italy/reshare/controller/MarketController.java
@@ -13,7 +13,6 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/market")
-@CrossOrigin(origins = "http://localhost:4200")
 public class MarketController {
     private final MarketService marketService;
 
