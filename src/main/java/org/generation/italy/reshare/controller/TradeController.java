50cc469e41ab51a675535a2934251914ad6a16f2
Stanislao Tariffa <stany.tariffa@gmail.com>
2024-06-19 11:07:21 +0200

    conclude il metodo add in TradeController

--- src/main/java/org/generation/italy/reshare/controller/TradeController.java
+++ src/main/java/org/generation/italy/reshare/controller/TradeController.java
@@ -6,6 +6,8 @@ import org.generation.italy.reshare.model.AppUser;
 import org.generation.italy.reshare.model.Item;
 import org.generation.italy.reshare.model.ItemTrade;
 import org.generation.italy.reshare.model.UserPrincipal;
+import org.generation.italy.reshare.model.services.abstractions.AppUserService;
+import org.generation.italy.reshare.model.services.abstractions.ItemService;
 import org.generation.italy.reshare.model.services.abstractions.TradeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -20,20 +22,25 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/trade")
 public class TradeController {
     private TradeService tradeService;
+    private AppUserService appUserService;
+    private ItemService itemService;
 
     @Autowired
-    public TradeController(TradeService tradeService) {
+    public TradeController(TradeService tradeService, AppUserService appUserService, ItemService itemService) {
         this.tradeService = tradeService;
+        this.appUserService = appUserService;
+        this.itemService = itemService;
     }
 
     @PostMapping("/add")
     public ResponseEntity<?> addItemTrade(@RequestBody ItemTradeDto itemTradeDto) throws EntityNotFoundException {
         try {
-            Item requestedItem = tradeService.findItemById(itemTradeDto.getRequestedItemId());
-            Item exchangedItem = tradeService.findItemById(itemTradeDto.getExchangedItemId());
-            AppUser requestingUser = tradeService.getUserByEmail(itemTradeDto.getRequestingUserEmail());
-            AppUser homeUser = tradeService.getUserByEmail(itemTradeDto.getHomeUserEmail());
-
+            Item requestedItem = itemService.findItemById(itemTradeDto.getRequestedItemId());
+            Item exchangedItem = itemService.findItemById(itemTradeDto.getExchangedItemId());
+            AppUser requestingUser = appUserService.getUserByEmail(itemTradeDto.getRequestingUserEmail());
+            AppUser homeUser = appUserService.getUserByEmail(itemTradeDto.getHomeUserEmail());
+            System.out.println(homeUser);
+            System.out.println(requestingUser);
             ItemTrade itemTrade = itemTradeDto.toItemTrade(requestedItem, requestingUser, exchangedItem, homeUser);
             ItemTrade savedItemTrade = tradeService.saveItemTrade(itemTrade);
             ItemTradeDto tradeDto = new ItemTradeDto(savedItemTrade);
